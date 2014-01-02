1a8ac3ce560ae49ee99e5f4c11c2d421e47c301d
graceact1989 <graceact1989@gmail.com>
2014-01-02 18:29:44 +0800

    add template source

--- DiaryText/grails-app/conf/DataSource.groovy
+++ DiaryText/grails-app/conf/DataSource.groovy
@@ -1,32 +1,57 @@
 dataSource {
     pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "root"
+    password = "root"
+    properties {
+        maxActive = 50
+        maxIdle = 25
+        minIdle = 5
+        initialSize = 5
+        minEvictableIdleTimeMillis = 60000
+        timeBetweenEvictionRunsMillis = 60000
+        maxWait = 10000
+        validationQuery = "/* PING */ SELECT 1"
+        // testOnBorrow=true
+        testOnReturn=true
+        testWhileIdle=true
+        numTestsPerEvictionRun=3
+    }
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
     cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+    jdbc.batch_size=1000
+    flush.mode='commit'
+    //hibernate.show_sql = true
 }
+
 // environment specific settings
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:devDB"
+            dbCreate = "update" // one of 'create', 'create-drop','update'
+            url = "jdbc:mysql://192.168.1.120:3306/diary_text?useUnicode=true&characterEncoding=UTF-8"
+//            url = "jdbc:mysql://192.168.1.198:3306/eopoosp?useUnicode=true&characterEncoding=UTF-8"
+            //url="jdbc:hsqldb:mem:devDb"
         }
     }
     test {
         dataSource {
+            //username = "sa"
+            //password = ""
             dbCreate = "update"
-            url = "jdbc:hsqldb:mem:testDb"
+            //url = "jdbc:hsqldb:mem:testDb"
+            url = "jdbc:mysql://192.168.1.120:3306/diary_text?useUnicode=true&characterEncoding=UTF-8"
+//            url = "jdbc:mysql://192.168.1.198:3306/eopoosp?useUnicode=true&characterEncoding=UTF-8"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+            url = "jdbc:mysql://192.168.1.120:3306/diary_text?useUnicode=true&characterEncoding=UTF-8"
+//            url = "jdbc:mysql://192.168.1.198:3306/eopoosp?useUnicode=true&characterEncoding=UTF-8"
         }
     }
 }
--- DiaryText/grails-app/domain/diarytext/Diary.groovy
+++ DiaryText/grails-app/domain/diarytext/Diary.groovy
@@ -3,5 +3,18 @@ package diarytext
 class Diary {
 
     static constraints = {
+        user()
+        text()
+        lastModify()
+    }
+
+    static belongsTo = [user:User]
+
+    int parentId;
+    String text;
+    Date lastModify;
+
+    public String toString(){
+        return user.userName+"\r\n"+text;
     }
 }
